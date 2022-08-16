4e061a34d933fbf141dfbb7c17111530a044bf1c
wuhengyu <578060214@qq.com>
2022-08-16 15:38:18 +0800

    update files

--- babys/settings.py
+++ babys/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'babys',
         'USER': 'root',
         'PASSWORD': '123456',
-        'HOST': '192.168.195.139',
+        'HOST': '192.168.195.141',
         'PORT': '3306',
 
     }
@@ -130,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- babys/urls.py
+++ babys/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('media/(?P<path>.*)', serve, {'document_root': settings.MEDIA_ROOT}, name='media')
 ]
